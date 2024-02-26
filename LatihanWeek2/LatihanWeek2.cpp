92f2fa2fff7ed0d0070a1e2aee04f186b893f888
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-02-26 17:40:07 +0700

    Add project files.

---
+++ LatihanWeek2/LatihanWeek2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//begin
+	//numeric nSisi,nLuas
+	//display 'masukan panjang sisi ='
+	//accept nSisi
+	//compute nLuas= nSisi * nSisi
+	//display 'luas persegi adalah = ' + nLuas
+
+	int nSisi, nLuas;
+	cout << "Masukan panjang sisi=";
+	cin >> nSisi;
+	nLuas = nSisi * nSisi;
+	cout << "luas persegi adalah =" << nLuas << endl;
+}//end
